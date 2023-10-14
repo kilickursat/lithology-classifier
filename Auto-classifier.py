76a26c444eb300c0dec37bd3fb6c9ab027060b47
Kursat Kilic <kilic_kursat@hotmail.com>
2023-10-14 18:51:28 +0900

    Update Auto-classifier.py

--- Auto-classifier.py
+++ Auto-classifier.py
@@ -34,7 +34,26 @@ def run():
 
 # Create inputs for the different features of the data
     if add_selectbox == 'Online':
-        pressure_gauge1 = st.number_input('Pressure gauge 1 (kPa)', min_value=float(df['pressure_gauge1'].min()), value=0)
+        pressure_gauge1 = st.number_input('Pressure gauge 1 (kPa)', min_value=0.0, max_value=210.27, value=64.95)
+        pressure_gauge2 = st.number_input('Pressure gauge 2 (kPa)', min_value=0.0, max_value=259.0, value=60.85)
+        pressure_gauge3 = st.number_input('Pressure gauge 3 (kPa)', min_value=0.0, max_value=175.36, value=58.52)
+        pressure_gauge4 = st.number_input('Pressure gauge 4 (kPa)', min_value=0.0, max_value=426.93, value=67.23)
+        digging_velocity_left = st.number_input('Digging velocity left (mm/min)', min_value=0.0, max_value=336.0, value=13.75)
+        digging_velocity_right = st.number_input('Digging velocity right (mm/min)', min_value=0.0, max_value=239.0, value=13.27)
+        shield_jack_stroke_left = st.number_input('Shield jack stroke left (mm)', min_value=0.0, max_value=3504.20, value=694.17)
+        shield_jack_stroke_right = st.number_input('Shield jack stroke right (mm)', min_value=0.0, max_value=5946.13, value=696.96)
+        propulsion_pressure = st.number_input('Propulsion pressure (MPa)', min_value=-81.87, max_value=31.55, value=15.60)
+        total_thrust = st.number_input('Total thrust (kN)', min_value=-19870.27, max_value=7160.73, value=3424.15)
+        cutter_torque = st.number_input('Cutter torque (kNm)', min_value=0.0, max_value=694.91, value=482.25)
+        cutterhead_rotation_speed = st.number_input('Cutterhead rotation speed (rpm)', min_value=0.0, max_value=7.00, value=1.97)
+        screw_pressure = st.number_input('Screw pressure (MPa)', min_value=-114.64, max_value=7.69, value=2.86)
+        screw_rotation_speed = st.number_input('Screw rotation speed (rpm)', min_value=-1.22, max_value=78.25, value=0.90)
+        gate_opening = st.number_input('Gate opening (%)', min_value=0.0, max_value=1.60, value=0.18)
+        mud_injection_pressure = st.number_input('Mud injection pressure (MPa)', min_value=0.0, max_value=58.32, value=16.33)
+        add_mud_flow = st.number_input('Add mud flow (L/min)', min_value=0.0, max_value=560.64, value=96.99)
+        back_in_injection_rate = st.number_input('Back in injection rate (%)', min_value=0.0, max_value=100.0, value=0.19)
+        """
+        pressure_gauge1 = st.number_input('Pressure gauge 1 (kPa)', min_value=float(['pressure_gauge1'].min()), value=0)
         pressure_gauge2 =st.number_input('Pressure gauge 2 (kPa)', min_value=float(df['pressure_gauge2'].min()), value=0)
         pressure_gauge3 = st.number_input('Pressure gauge 3 (kPa)', min_value=float(df['pressure_gauge3'].min()), value=0)
         pressure_gauge4 =st.number_input('Pressure gauge 4 (kPa)', min_value=float(df['pressure_gauge4'].min()), value=0)
@@ -52,7 +71,7 @@ def run():
         mud_injection_pressure = st.number_input('Mud injection pressure (MPa)', min_value=float(df['mud_injection_pressure'].min()), value=0)
         add_mud_flow = st.number_input('Add mud flow (L/min)', min_value=float(df['add_mud_flow'].min()), value=0)
         back_in_injection_rate = st.number_input('Back in injection rate (%)', min_value=float(df['back_in_injection_rate'].min()), max_value=100, value=0)
-        
+        """
         output=""
 
         input_dict = {
